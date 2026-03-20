bc4ca9151380ac368afcd48d87a1e67708ce4db4
DucMinhnguyen-cmd <nagato.minh@gmail.com>
2026-03-20 18:20:00 +0700

    tiếp tục

--- cleaning_missing.R
+++ cleaning_missing.R
@@ -18,6 +18,53 @@ if(any(is.na(d1$MasVnrType))) {
  d1$MasVnrType[is.na(d1$MasVnrType)]<-names(table(d1$MasVnrType))[which.max(table(d1$MasVnrType))]
 }
 
-#sử dụng yếu vị với BsmtQual
+#sử dụng yếu vị với BsmtQual, bsmtCond, BsmtExposure, BsmtFinType1, BsmtFinType2
+# 1. Định nghĩa hàm tìm Yếu vị
+get_mode <- function(x) {
+  x <- x[!is.na(x)] # Bỏ qua NA khi đếm
+  if (length(x) == 0) return(NA) 
+  bang_tan_suat <- table(x)
+  return(names(bang_tan_suat)[which.max(bang_tan_suat)])
+}
+cols_to_mode <- c("BsmtQual", "BsmtCond", "BsmtExposure", "BsmtFinType1", "BsmtFinType2")
 
+d1[cols_to_mode] <- lapply(d1[cols_to_mode], function(x) {
+  if (any(is.na(x))) {
+    x[is.na(x)] <- get_mode(x)
+  }
+  return(x)
+})
+#cũng như vạy nhưng với FireplaceQu, GarageType, GarageFinish, GarageQual,  GarageCond
+cols_to_mode <- c("FireplaceQu", "GarageType", "GarageFinish", "GarageQual",  "GarageCond")
 
+d1[cols_to_mode] <- lapply(d1[cols_to_mode], function(x) {
+  if (any(is.na(x))) {
+    x[is.na(x)] <- get_mode(x)
+  }
+  return(x)
+})
+#tính trung vị garageỶBlt
+if(any(is.na(d1$GarageYrBlt))) {
+  d1$GarageYrBlt[is.na(d1$GarageYrBlt)] <- median(d1$GarageYrBlt, na.rm = T)
+}
+colSums(is.na(d1))
+
+#điền giá trị "none" vào poolQC
+if(any(is.na(d1$PoolQC))) {
+  d1$PoolQC[is.na(d1$PoolQC)] <- "None"
+}
+#
+cols_to_mode <- c("Fence")
+
+d1[cols_to_mode] <- lapply(d1[cols_to_mode], function(x) {
+  if (any(is.na(x))) {
+    x[is.na(x)] <- get_mode(x)
+  }
+  return(x)
+})
+#
+if(any(is.na(d1$MiscFeature))) {
+  d1$MiscFeature[is.na(d1$MiscFeature)] <- "None"
+}
+
+colSums(is.na(d1))
